ba8605f630595bad745e3705f852a48edb80514d
Andrea Ros <a.ros@donboscosandona.it>
2022-02-08 11:33:10 +0100

    per Riccardo

---
+++ React/02/index.js
@@ -0,0 +1,19 @@
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Card />
+                </div>
+                <div className="col">
+                    <Card />
+                </div>
+                <div className="col">
+                    <Card />
+                </div>
+            </div>
+        </div>
+    </div>,
+    document.getElementById('root')
+)
